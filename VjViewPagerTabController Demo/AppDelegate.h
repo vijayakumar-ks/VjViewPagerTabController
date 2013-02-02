8ddc201f6bb78b511feed4f0bbf1217e309c6a07
Vijay <Visush@Visushs-Mac-Pro.local>
2013-02-02 14:17:49 +0530

    Initial Commit

---
+++ VjViewPagerTabController Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VjViewPagerTabController Demo
+//
+//  Created by Vijay on 02/02/13.
+//  Copyright (c) 2013 vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VjViewPagerTabController Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VjViewPagerTabController Demo
+//
+//  Created by Vijay on 02/02/13.
+//  Copyright (c) 2013 vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
